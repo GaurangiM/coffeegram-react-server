2736c57fdf8f6f503edec0d04258136109e35efa
GaurangiM <manegaurangi89@gmail.com>
2021-06-17 17:32:28 +0200

    Add model and migration file for user_cafes table

---
+++ models/user_cafe.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user_cafe extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user_cafe.init({
+    review: { type: DataTypes.TEXT, allowNull: false},
+    rating: { type: DataTypes.INTEGER, allowNull: false},
+    visitDate: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'user_cafe',
+  });
+  return user_cafe;
+};
