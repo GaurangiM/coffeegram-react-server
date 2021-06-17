47d5d57cebe2b3718670267e6bc082f4cc9842da
GaurangiM <manegaurangi89@gmail.com>
2021-06-17 16:37:12 +0200

    Add model and migration file for address table

---
+++ models/address.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  address.init({
+    postCode: { type: DataTypes.INTEGER, allowNull: false},
+    houseNumber: { type: DataTypes.INTEGER, allowNull: false, unique: true},
+    houseNumberExt: { type: DataTypes.INTEGER},
+    streetName: { type: DataTypes.STRING, allowNull: false},
+    city: { type: DataTypes.STRING, allowNull: false},
+    latitude: { type: DataTypes.FLOAT, allowNull: false},
+    longitude: { type: DataTypes.FLOAT, allowNull: false}
+  }, {
+    sequelize,
+    modelName: 'address',
+  });
+  return address;
+};
