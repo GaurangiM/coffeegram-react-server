ed17e313c03edc63d8d157f9f505c80e3d8e5b86
GaurangiM <manegaurangi89@gmail.com>
2021-06-18 15:08:17 +0200

    Edit seeder file to add down action

--- seeders/20210618101747-some-images.js
+++ seeders/20210618101747-some-images.js
@@ -99,11 +99,7 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('images', null, {});
+    
   }
 };
